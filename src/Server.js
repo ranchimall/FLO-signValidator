ac21c53d88aa88792f230f3429af988bff452b03
sairajzero <sairaj0630@gmail.com>
2021-08-03 19:41:06 +0530

    Update Server.js

--- src/Server.js
+++ src/Server.js
@@ -1,10 +1,10 @@
 const http = require('http');
 const floCrypto = require('./floCrypto');
 
-module.exports = function startServer(port){
+module.exports = function startServer(port) {
     const httpServer = http.createServer((req, res) => {
         res.setHeader("Access-Control-Allow-Origin", "*");
-        if(req.method === "GET")
+        if (req.method === "GET")
             res.end("");
         else if (req.method === "POST") {
             //POST: All data processing (required JSON input)
@@ -12,20 +12,22 @@ module.exports = function startServer(port){
             req.on('data', chunk => data += chunk);
             req.on('end', () => {
                 //process verification
-                let result = {};
+                let result = {
+                    success: false
+                };
                 try {
                     var d = JSON.parse(data);
-                    if(!floCrypto.validateAddr(d.floID))
-                        result.failed = "Invalid floID";
-                    else if(floCrypto.getFloID(d.pubKey) !== d.floID)
-                        result.failed = "Public key mismatched";
+                    if (!floCrypto.validateAddr(d.floID))
+                        result.reason = "Invalid floID";
+                    else if (floCrypto.getFloID(d.pubKey) !== d.floID)
+                        result.reason = "Public key mismatched";
                     else if (!floCrypto.verifySign(d.message, d.sign, d.pubKey))
-                        result.failed = "Signature not verified";
+                        result.reason = "Signature not verified";
                     else
                         result.success = true;
                 } catch (error) {
-                    result.Error = "Invalid request";
-                }finally{
+                    result.reason = "Invalid request";
+                } finally {
                     res.end(JSON.stringify(result));
                 };
             });
